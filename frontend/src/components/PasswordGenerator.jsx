04ec5633d8784c94d990cc672186ba9dabcc7353
paigauresh <paigauresh@gmail.com>
2025-09-06 12:52:51 +0530

    style(frontend): Set background to white for input field in PasswordGenerator

--- frontend/src/components/PasswordGenerator.jsx
+++ frontend/src/components/PasswordGenerator.jsx
@@ -42,7 +42,7 @@ const PasswordGenerator = () => {
         <label className="block mb-2 text-sm font-medium text-gray-900 text-black">Password Length:</label>
         <input
           type="number"
-          className="w-full px-3 py-2 placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          className="w-full px-3 py-2 placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-white"
           value={length}
           onChange={(e) => setLength(parseInt(e.target.value))}
           min="4"
